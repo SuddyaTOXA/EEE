841598513c7ffde445a66b8ad1391e70cee7263b
Anton <suddyadred@gmail.com>
2018-03-10 21:45:31 +0200

    suddya

--- header.php
+++ header.php
@@ -6,11 +6,11 @@
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_bloginfo('template_url'); ?>/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_bloginfo('template_url'); ?>/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_bloginfo('template_url'); ?>/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo get_bloginfo('template_url'); ?>/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo get_bloginfo('template_url'); ?>/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_bloginfo('template_url'); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_bloginfo('template_url'); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_bloginfo('template_url'); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_bloginfo('template_url'); ?>/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_bloginfo('template_url'); ?>/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
